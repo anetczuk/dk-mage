faa16d33ca42aac10514a13d5d658fe7bcec53c9
anetczuk <anetczuk@o2.pl>
2021-05-25 22:35:17 +0200

    Add tools

---
+++ configure/clang_release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+BUILD_TYPE=Release
+
+
+SRC_DIR=$SCRIPT_DIR/../src
+
+BUILD_DIR_NAME=$(echo $BUILD_TYPE"_clang" | tr '[:upper:]' '[:lower:]')
+BUILD_DIR=$SCRIPT_DIR/../build/$BUILD_DIR_NAME
+
+
+## remove old configuration
+rm -rf $BUILD_DIR 
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
+cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE $SRC_DIR
