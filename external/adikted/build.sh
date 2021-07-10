a1a71a43d27c470a9f10876b4c8459fb51036a36
anetczuk <anetczuk@o2.pl>
2021-07-10 21:44:22 +0200

    Update build script

--- external/adikted/build.sh
+++ external/adikted/build.sh
@@ -13,23 +13,31 @@ SRC_ARCHIVE=ADiKtEd-master.zip
 
 
 SRC_DIR=$(basename -s .zip $SRC_ARCHIVE)
-BUILD_PATH="$SCRIPT_DIR/build/$SRC_DIR"
+UNZIP_DIR="$SCRIPT_DIR/build"
+BUILD_PATH="$UNZIP_DIR/$SRC_DIR"
 INSTALL_PATH="$SCRIPT_DIR/install"
 LIB_PATH="$BUILD_PATH/libadikted"
 
-rm -rf "$BUILD_PATH"
-rm -rf "$INSTALL_PATH"
 
-mkdir -p "$BUILD_PATH"
-mkdir -p "$INSTALL_PATH"
-mkdir -p "$INSTALL_PATH/include/libadikted"
-mkdir -p "$INSTALL_PATH/lib"
+unzip_src() {
+    rm -rf "$BUILD_PATH"
+    rm -rf "$INSTALL_PATH"
+    
+    mkdir -p "$BUILD_PATH"
+    mkdir -p "$INSTALL_PATH"
+    mkdir -p "$INSTALL_PATH/include/libadikted"
+    mkdir -p "$INSTALL_PATH/lib"
+    
 
+    unzip -o "$SCRIPT_DIR/$SRC_ARCHIVE" -d "$UNZIP_DIR"
+    
+    ## cpy fixes
+    if [ -d "$SCRIPT_DIR/fix" ]; then
+        cp $SCRIPT_DIR/fix/* "$LIB_PATH"
+    fi
+}
 
-unzip -o "$SCRIPT_DIR/$SRC_ARCHIVE" -d "$SCRIPT_DIR/build"
-
-## cpy fixes
-cp $SCRIPT_DIR/fix/* "$LIB_PATH"
+unzip_src
 
 
 cd "$LIB_PATH"
@@ -38,31 +46,38 @@ cd "$LIB_PATH"
 cp $LIB_PATH/*.h "$INSTALL_PATH/include/libadikted"
 
 
-## building for Windows
-make clean
-make all
+build_windws() {
+    ## building for Windows
+    make clean
+    make all
+    
+    cp "$LIB_PATH/bin/adikted.dll" "$INSTALL_PATH/lib" && true
+    
+    
+    make clean
+    make all DEBUG=1
+    
+    cp "$LIB_PATH/bin/adikted.dll" "$INSTALL_PATH/lib/adikted_d.dll" && true
+}
 
-cp "$LIB_PATH/bin/adikted.dll" "$INSTALL_PATH/lib" && true
-
-
-make clean
-make all DEBUG=1
-
-cp "$LIB_PATH/bin/adikted.dll" "$INSTALL_PATH/lib/adikted_d.dll" && true
-
-
-
-## building for Linux
-make clean
-make all BUILD_FOR_LINUX=1
-
-cp "$LIB_PATH/bin/adikted.so" -T "$INSTALL_PATH/lib/libadikted.so" && true
+build_windws
 
 
-make clean
-make all BUILD_FOR_LINUX=1 DEBUG=1
+build_linux() {
+    ## building for Linux
+    make clean
+    make all BUILD_FOR_LINUX=1
+    
+    cp "$LIB_PATH/bin/adikted.so" -T "$INSTALL_PATH/lib/libadikted.so" && true
+    
+    
+    make clean
+    make all BUILD_FOR_LINUX=1 DEBUG=1
+    
+    cp "$LIB_PATH/bin/adikted.so" -T "$INSTALL_PATH/lib/libadikted_d.so" && true
+}
 
-cp "$LIB_PATH/bin/adikted.so" -T "$INSTALL_PATH/lib/libadikted_d.so" && true
+build_linux
 
 
 echo -e "\nbuilding adikted done"
